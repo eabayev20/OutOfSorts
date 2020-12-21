46edf2ac30cbf345d457ae83947f23015a5ea8e6
eabayev20 <eabayev20@gmail.com>
2020-12-20 22:33:49 -0500

    bubble sort

--- Sorts.java
+++ Sorts.java
@@ -1,5 +1,13 @@
 public class Sorts{}
 public static void bubbleSort (int[] data) {
-
+  for (int i = 0; i<data.length; i++ ) {
+         for (int j = 0; j<data.length-1-i; j++) {
+             if (data[j]>data[j+1]) {
+                 int x = data[j];
+                 data[j] = data[j+1];
+                 
+               }
+           }
+       }
 }
 }
