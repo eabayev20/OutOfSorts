f3aebf6e98a9f9cd1abf65ea5efb4b625b26e578
eabayev20 <eabayev20@gmail.com>
2021-01-03 20:42:16 -0500

    selection fixed

--- Sorts.java
+++ Sorts.java
@@ -13,21 +13,22 @@ public static void bubbleSort (int[] data) {
 public static void selectionSort(int[] data){
 
   for (int i = 0; i != data.length; i++) {
-    int tempdex = i;
-    int temp = 0;
-    int temp2 = data[i];
+    int tx = i;
+    int t = 0;
+    int t2 = data[i];
     for (int j = 0; j != data.length-1; j++) {
 
 
     if (data[i] > data[j+1]) {
-      temp = data[j+1];
-      tempdex = j+1;
+      t = data[j+1];
+      tx = j+1;
 
     }
     }
-    data[i] = temp;
-    data[tempdex] = temp2;
+    data[i] = t;
+    data[tx] = t2;
   }
 
 }
+
 }
